3b74e64e28894147921c5169a8513b91a13d90d2
Marcos A C Paula <cardepaula@gmail.com>
2018-11-21 10:46:18 -0200

    Alteraçao do create e insert

--- sql/sql_alterado.sql
+++ sql/sql_alterado.sql
@@ -1,13 +1,12 @@
 /* criacao de estruturas */
-/* Modelo FÌsico */
-
+/* criacao de estruturas */
 CREATE TABLE USUARIO (
     id_usuario smallserial PRIMARY KEY,
-    numero_cartao char(16);
+    numero_cartao char(16),
     id_logradouro smallserial,
     saldo money,
     email varchar(100),
-    senha char(64),
+    senha varchar(64),
     nascimento date,
     cpf char(11),
     nome varchar(150)
@@ -34,8 +33,7 @@ CREATE TABLE CIDADE (
 
 CREATE TABLE LINHA (
     numero_linha smallserial PRIMARY KEY,
-    desc_linha varchar(100),
-    sentido char(1)
+    desc_linha varchar(100)
 );
 
 CREATE TABLE HORARIO (
@@ -56,12 +54,14 @@ CREATE TABLE CARTAO (
     numero char(16) PRIMARY KEY,
     id_usuario smallserial,
     titular varchar(150),
-    validade date
+    validade char(7)
 );
 
 CREATE TABLE ITINERARIO (
     id_itinerario smallserial PRIMARY KEY,
-    numero_linha smallserial
+    numero_linha smallserial,
+    destino varchar(50),
+    sentido char(1)
 );
 
 CREATE TABLE PAGAMENTO (
@@ -94,14 +94,12 @@ CREATE TABLE ITINERARIO_PONTO (
     id_ponto smallserial,
     pos_sequencia integer
 );
-
-
-/* adicionar chaves estrangeiras */ 
-ALTER TABLE USUARIO ADD CONSTRAINT FK_USUARIO_ 2
+ 
+/* adicionar chaves estrangeiras */
+ALTER TABLE USUARIO ADD CONSTRAINT FK_USUARIO_2
     FOREIGN KEY (numero_cartao)
-    REFERENCES cartao(numero);
-    
-
+    REFERENCES CARTAO (numero);
+ 
 ALTER TABLE USUARIO ADD CONSTRAINT FK_USUARIO_3
     FOREIGN KEY (id_logradouro)
     REFERENCES LOGRADOURO (id_logradouro);
@@ -158,55 +156,8 @@ ALTER TABLE ITINERARIO_PONTO ADD CONSTRAINT FK_ITINERARIO_PONTO_2
     FOREIGN KEY (id_ponto)
     REFERENCES PONTO (id_ponto);
 
----- Insercao de dados ----
 
 ---- Insercao de dados ----
-
-/* usuario */
-INSERT INTO USUARIO (id_usuario, cpf, nome, nascimento, email, senha, id_logradouro, saldo, id_cartao)
-    VALUES(1,'11122233344','Joao da Silva','1992-12-20','joaodasilva@yahoo.com.br','a1b2c3d4','1', 12.80,1),
-    (2,'11199988822','Jadson Teco','1996-10-10','jadteco@gmail.com','teco78910','2', 13.04,2),
-    (3,'22233344455','Antonio Santos','1988-10-20','antoniosantos@hotmail.com','123456','3', 17.17,3),
-    (4,'12332145690','Amaro Neto','1982-07-01','amaroneto@gmail.com','abc123456','7', 18.81,4),
-    (5,'12345612399','Natalia Vieira','1997-11-17','nataliavieira@gmail.com','aaa123bbb','10', 14.07,5),
-    (6,'11111122288','Izabela Felix','1995-04-23','izabelafelix@gmail.com','abcd9182','4', 07.14,6),
-    (7,'22211144477','Gabriel Fernandes','1994-05-11','gabrielfernandes@hotmail.com','lalala721','9', 21.05,7),
-    (8,'78945612300','Diego Bolsoni','1993-01-04','diegobolsoni@yahoo.com.br','diego123456','8', 13.73,8),
-    (9,'44412344466','Ingrid Brum','1998-10-21','ingridbrum@gmail.com','brum010203','5', 14.50,9),
-    (10,'98788455611','Carolina Uliana','1997-06-18','caroluliana@hotmail.com','carol78690','6', 3.40,10);
-
-
-/* cartao */
-INSERT INTO CARTAO (numero, titular, validade, id_usuario)
-    VALUES('1111222233330000','Joao da Silva','06/2020', 1),
-    ('1111222233331111','Jadson Teco','07/2019', 2),
-    ('1111222233332222','Antonio Santos','07/2021', 3),
-    ('1111222233333333','Amaro Neto','03/2024', 4),
-    ('1111222233334444','Natalia Vieira','06/2021', 5),
-    ('1111222233335555','Izabela Felix','04/2023', 6),
-    ('1111222233336666','Gabriel Fernandes','05/2022', 7),
-    ('1111222233337777','Diego Bolsoni','03/2020', 8),
-    ('1111222233338888','Ingrid Brum','08/2024', 9),
-    ('1111222233339999','Carolina Uliana','08/2022', 10);
-
-
-/* linha */
-INSERT INTO LINHA (numero_linha, desc_linha, sentido)
-    VALUES(815,'IFES / T. LARANJEIRAS',1),
-    (507,'T. LARANJEIRAS / T. IBES VIA 3™ PONTE/RETA DA PENHA',2),
-    (650,'T.VILA VELHA/T.IBES VIA PRAIA DE ITAPO√',1),
-    (611,'T.ITAPARICA/PRAIA DA COSTA VIA ITAPO√/CREFES',1),
-    (662,'T.VILA VELHA/PRAIA DA COSTA VIA CREFES - CIRCULAR',1),
-    (508,'T.LARANJEIRAS/T.ITAPARICA VIA T.VILA VELHA/3™ PONTE/CAMBURI',2),
-    (551,'T.JACARAIPE/T.ITAPARICA VIA T.CARAPINA/3™ PONTE',1),
-    (501,'T.JACARAIPE/T.ITAPARICA VIA T.CARAPINA/3™ PONTE/T.VILA VELHA',1),
-    (560,'T.LARANJEIRAS/T.ITAPARICA VIA CAMBURI/3™ PONTE',2),
-    (520,'T.CARAPINA/T.VILA VELHA VIA RETA DA PENHA/3™ PONTE',1);
-
-
-
-INSERT INTO LINHA (numero_linha, desc_linha, sentido)
-    VALUES(516,'T. IBES/ T. JACARAIPE',1);
 
 /* cidade */
 INSERT INTO CIDADE (id_cidade, desc_cidade)
@@ -219,11 +170,11 @@ INSERT INTO CIDADE (id_cidade, desc_cidade)
     (7,'Aracruz'),
     (8,'Itapemirim'),
     (9,'Colatina'),
-    (10,'Fund„o');
+    (10,'Fund√£o');
 
 /* bairro */
 INSERT INTO BAIRRO (id_bairro, desc_bairro, id_cidade)
-    VALUES(1,'Itapu„',1),
+    VALUES(1,'Itapu√£',1),
     (2,'Praia da Costa',1),
     (3,'Jardim da Penha',2),
     (4,'Praia do Canto',2),
@@ -238,23 +189,48 @@ INSERT INTO BAIRRO (id_bairro, desc_bairro, id_cidade)
 /* logradouro */
 INSERT INTO LOGRADOURO (id_logradouro, cep, desc_tipo, desc_logradouro, id_bairro)
     VALUES(1,'29146440','Rua','Curitiba',1),
-    (2,'29465824','Avenida','S„o Paulo',2),
+    (2,'29465824','Avenida','S√£o Paulo',2),
     (3,'29753159','Avenida','Hugo Musso',2),
     (4,'29123456','Rua','Amoreira',1),
     (5,'29456123','Avenida','Nossa Senhora Da Penha',4),
     (6,'29234567','Avenida','Rio Branco',4),
-    (7,'29200485','Rua','TrÍs',7),
+    (7,'29200485','Rua','Tr√™s',7),
     (8,'29001407','Rua','Ayrton Senna',7),
     (9,'29582471','Rua','Humberto Pereira',6),
     (10,'29269962','Travessa','Belina',10);
 
+/* ponto */
+INSERT INTO PONTO (id_ponto,id_logradouro,longitude, latitude)
+    VALUES(1, 5, -20.28880,-40.30392),
+    (2, 5, -20.28918,-40.30349),
+    (3, 5, -20.29302,-40.30220),
+    (4, 5, -20.29361,-40.30167),
+    (5, 5, -20.29537,-40.30115),
+    (6, 5, -20.29698,-40.30051),
+    (7, 5, -20.29821,-40.30000),
+    (8, 5, -20.29897,-40.29951),
+    (9, 5, -20.30258,-40.29828),
+    (10, 5, -20.30220,-40.29821);
 
 /* tipo_horario */
 INSERT INTO TIPO_HORARIO (id_tipo, desc_horario)
-    VALUES(1,'DIAS ⁄TEIS'),
-    (2,'S¡BADOS'),
+    VALUES(1,'DIAS √öTEIS'),
+    (2,'S√ÅBADOS'),
     (3,'DOMINGOS E FERIADOS'),
-    (4,'ATÕPICOS ENTRE FERIADOS');
+    (4,'AT√çPICOS ENTRE FERIADOS');
+
+/* linha */
+INSERT INTO LINHA (numero_linha, desc_linha)
+    VALUES(815,'IFES / T. LARANJEIRAS'),
+    (507,'T. LARANJEIRAS / T.IBES VIA 3¬™ PONTE/RETA DA PENHA'),
+    (650,'T.VILA VELHA/T.IBES VIA PRAIA DE ITAPO√É'),
+    (611,'T.ITAPARICA/PRAIA DA COSTA VIA ITAPO√É/CREFES'),
+    (662,'T.VILA VELHA/PRAIA DA COSTA VIA CREFES - CIRCULAR'),
+    (508,'T.LARANJEIRAS/T.ITAPARICA VIA T.VILA VELHA/3¬™ PONTE/CAMBURI'),
+    (551,'T.JACARAIPE/T.ITAPARICA VIA T.CARAPINA/3¬™ PONTE'),
+    (501,'T.JACARAIPE/T.ITAPARICA VIA T.CARAPINA/3¬™ PONTE/T.VILA VELHA'),
+    (560,'T.LARANJEIRAS/T.ITAPARICA VIA CAMBURI/3¬™ PONTE'),
+    (520,'T.CARAPINA/T.VILA VELHA VIA RETA DA PENHA/3¬™ PONTE');
 
 /* horario */
 /* 815 */
@@ -292,125 +268,51 @@ INSERT INTO HORARIO (id_horario, hora_saida, id_tipo , data_inicio, numero_linha
     (31,'22:10:00',1,'2017-05-28',815,'IFES');
 /* 507 */
 INSERT INTO HORARIO (id_horario, hora_saida, id_tipo , data_inicio, numero_linha, desc_terminal)
-    VALUES(55,'05:00:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (32,'05:20:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (33,'05:35:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (34,'05:45:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (35,'06:04:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (36,'06:13:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (37,'06:22:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (38,'06:31:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (39,'06:40:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (40,'06:49:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (41,'06:58:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (42,'07:07:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (43,'07:17:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (44,'07:27:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (45,'07:37:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (46,'07:48:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (47,'08:00:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (48,'08:17:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (49,'08:34:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (50,'08:52:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (51,'09:10:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (52,'09:30:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (53,'09:50:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
-    (54,'10:10:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS');
-    
-    
-INSERT INTO HORARIO (id_horario, hora_saida, id_tipo , data_inicio, numero_linha, desc_terminal)
-    VALUES
-    (56,'05:20:00',1,'2017-01-13',516,'T. IBES'),
-    (57,'05:35:00',1,'2017-01-13',516,'T. IBES'),
-    (58,'05:45:00',1,'2017-01-13',516,'T. IBES'),
-    (59,'06:04:00',1,'2017-01-13',516,'T. IBES'),
-    (60,'06:13:00',1,'2017-01-13',516,'T. IBES'),
-    (61,'06:22:00',1,'2017-01-13',516,'T. IBES'),
-    (62,'06:31:00',1,'2017-01-13',516,'T. IBES'),
-    (63,'06:40:00',1,'2017-01-13',516,'T. IBES'),
-    (64,'06:49:00',1,'2017-01-13',516,'T. IBES'),
-    (65,'06:58:00',1,'2017-01-13',516,'T. IBES'),
-    (66,'07:07:00',1,'2017-01-13',516,'T. IBES'),
-    (67,'07:17:00',1,'2017-01-13',516,'T. IBES'),
-    (68,'07:27:00',1,'2017-01-13',516,'T. IBES'),
-    (69,'07:37:00',1,'2017-01-13',516,'T. IBES'),
-    (70,'07:48:00',1,'2017-01-13',516,'T. IBES'),
-    (71,'08:00:00',1,'2017-01-13',516,'T. IBES'),
-    (72,'08:17:00',1,'2017-01-13',516,'T. IBES'),
-    (73,'08:34:00',1,'2017-01-13',516,'T. IBES'),
-    (74,'08:52:00',1,'2017-01-13',516,'T. IBES'),
-    (75,'09:10:00',1,'2017-01-13',516,'T. IBES'),
-    (76,'09:30:00',1,'2017-01-13',516,'T. IBES'),
-    (77,'09:50:00',1,'2017-01-13',516,'T. IBES'),
-    (78,'10:10:00',1,'2017-01-13',516,'T. IBES');
-
-
+    VALUES(41,'05:00:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (42,'05:20:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (43,'05:35:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (44,'05:45:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (45,'06:04:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (46,'06:13:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (47,'06:22:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (48,'06:31:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (49,'06:40:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (50,'06:49:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (51,'06:58:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (52,'07:07:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (53,'07:17:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (54,'07:27:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (55,'07:37:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (56,'07:48:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (57,'08:00:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (58,'08:17:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (59,'08:34:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (60,'08:52:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (61,'09:10:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (62,'09:30:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (63,'09:50:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS'),
+    (64,'10:10:00',1,'2017-10-15',507,'TERMINAL LARANJEIRAS');
 
 /* itinerario */
-INSERT INTO ITINERARIO (id_itinerario,numero_linha)
-    VALUES (1, 507),
-    (2, 520),
-    (3, 560),
-    (4, 508),
-    (5, 551),
-    (6, 650),
-    (7, 815),
-    (8, 650),
-    (9, 611),
-    (10, 815);
-    
-/* passagem 815 507 650 611 508 551 501 560 520*/
-INSERT INTO PASSAGEM (id_passagem, id_usuario, numero_linha, id_pagamento, data, hora, valor)
-    VALUES(1,3,815,10,'2018-10-08','07:30:00', 3.40),
-    (2,3,815,10,'2018-10-08','12:00:00', 3.40),
-    (3,3,815,10,'2018-10-09','07:30:00', 3.40),
-    (4,3,815,10,'2018-10-09','12:00:00', 3.40),
-    (5,3,815,10,'2018-10-10','07:30:00', 3.40),
-    (6,3,815,10,'2018-10-10','12:00:00', 3.40),
-    (7,3,815,10,'2018-10-11','07:30:00', 3.40),
-    (8,3,815,10,'2018-10-11','12:00:00', 3.40),
-    (9,3,815,10,'2018-10-12','07:30:00', 3.40),
-    (10,3,815,10,'2018-10-12','12:00:00', 3.40);
-    
-
-INSERT INTO PAGAMENTO (id_pagamento,numero_cartao, valor_pagamento, data_pagamento)
-    VALUES(1,'1111222233332222',100.00,'2018-01-08' ),
-    (2,'1111222233330000',100.00,'2018-02-08' ),
-    (3,'1111222233334444',100.00,'2018-03-08' ),
-    (4,'1111222233337777',100.00,'2018-04-08' ),
-    (5,'1111222233339999',100.00,'2018-05-08' ),
-    (6,'1111222233331111',100.00,'2018-06-08' ),
-    (7,'1111222233336666',100.00,'2018-07-08' ),
-    (8,'1111222233333333',100.00,'2018-08-08' ),
-    (9,'1111222233335555',100.00,'2018-09-08' ),
-    (10,'1111222233338888',100.00,'2018-10-08' );
-
-
-    /*
-    VALUES('1111222233330000','Joao da Silva','06/2020', 1),
-    ('1111222233331111','Jadson Teco','07/2019', 2),
-    ('1111222233332222','Antonio Santos','07/2021', 3),
-    ('1111222233333333','Amaro Neto','03/2024', 4),
-    ('1111222233334444','Natalia Vieira','06/2021', 5),
-    ('1111222233335555','Izabela Felix','04/2023', 6),
-    ('1111222233336666','Gabriel Fernandes','05/2022', 7),
-    ('1111222233337777','Diego Bolsoni','03/2020', 8),
-    ('1111222233338888','Ingrid Brum','08/2024', 9),
-    ('1111222233339999','Carolina Uliana','08/2022', 10);
-    */
-
-/* ponto */
-INSERT INTO PONTO (id_ponto,id_logradouro,longitude, latitude)
-    VALUES(1, 5, -20.28880,-40.30392),
-    (2, 5, -20.28918,-40.30349),
-    (3, 5, -20.29302,-40.30220),
-    (4, 5, -20.29361,-40.30167),
-    (5, 5, -20.29537,-40.30115),
-    (6, 5, -20.29698,-40.30051),
-    (7, 5, -20.29821,-40.30000),
-    (8, 5, -20.29897,-40.29951),
-    (9, 5, -20.30258,-40.29828),
-    (10, 5, -20.30220,-40.29821);
+INSERT INTO ITINERARIO (id_itinerario, numero_linha, destino, sentido)
+    VALUES (1, 507, 'T. LARANJEIRAS',1),
+    (2, 507, 'T. IBES',2),
+    (3, 815, 'IFES',1),
+    (4, 815, 'T. LARANJEIRAS',2),
+    (5, 611, 'T.ITAPARICA',1),
+    (6, 611, 'PRAIA DA COSTA',2),
+    (7, 662, 'T.VILA VELHA',1),
+    (8, 662, 'PRAIA DA COSTA',2),
+    (9, 501, 'T.JACARAIPE',1),
+    (10, 501, 'T.ITAPARICA',2),
+    (11, 508, 'T.LARANJEIRAS',1),
+    (12, 508, 'T.ITAPARICA',2),
+    (13, 551, 'T.JACARAIPE',1),
+    (14, 551, 'T.ITAPARICA',2),
+    (15, 560, 'T.LARANJEIRAS',1),
+    (16, 560, 'T.ITAPARICA',2),
+    (17, 520, 'T.CARAPINA',1),
+    (18, 520, 'T.VILA VELHA',2);
 
 /* itinerario_ponto */
 INSERT INTO ITINERARIO_PONTO (id_intin_ponto, id_itinerario, id_ponto, pos_sequencia)
@@ -424,6 +326,70 @@ INSERT INTO ITINERARIO_PONTO (id_intin_ponto, id_itinerario, id_ponto, pos_seque
     (8, 9, 6, 11),
     (9, 9, 8, 12),
     (10, 9, 10, 13);
+
+/* usuario */
+INSERT INTO USUARIO (id_usuario, cpf, nome, nascimento, email, senha, id_logradouro, saldo)
+    VALUES(1,'11122233344','Joao da Silva','1992-12-20','joaodasilva@yahoo.com.br','a1b2c3d4','1', 12.80),
+    (2,'11199988822','Jadson Teco','1996-10-10','jadteco@gmail.com','teco78910','2', 13.04),
+    (3,'22233344455','Antonio Santos','1988-10-20','antoniosantos@hotmail.com','123456','3', 17.17),
+    (4,'12332145690','Amaro Neto','1982-07-01','amaroneto@gmail.com','abc123456','7', 18.81),
+    (5,'12345612399','Natalia Vieira','1997-11-17','nataliavieira@gmail.com','aaa123bbb','10', 14.07),
+    (6,'11111122288','Izabela Felix','1995-04-23','izabelafelix@gmail.com','abcd9182','4', 07.14),
+    (7,'22211144477','Gabriel Fernandes','1994-05-11','gabrielfernandes@hotmail.com','lalala721','9', 21.05),
+    (8,'78945612300','Diego Bolsoni','1993-01-04','diegobolsoni@yahoo.com.br','diego123456','8', 13.73),
+    (9,'44412344466','Ingrid Brum','1998-10-21','ingridbrum@gmail.com','brum010203','5', 14.50),
+    (10,'98788455611','Carolina Uliana','1997-06-18','caroluliana@hotmail.com','carol78690','6', 3.40);
+
+/* cartao */
+INSERT INTO CARTAO (numero, titular, validade, id_usuario)
+    VALUES('1111222233330000','Joao da Silva','06/2020', 1),
+    ('1111222233331111','Jadson Teco','07/2019', 2),
+    ('1111222233332222','Antonio Santos','07/2021', 3),
+    ('1111222233333333','Amaro Neto','03/2024', 4),
+    ('1111222233334444','Natalia Vieira','06/2021', 5),
+    ('1111222233335555','Izabela Felix','04/2023', 6),
+    ('1111222233336666','Gabriel Fernandes','05/2022', 7),
+    ('1111222233337777','Diego Bolsoni','03/2020', 8),
+    ('1111222233338888','Ingrid Brum','08/2024', 9),
+    ('1111222233339999','Carolina Uliana','08/2022', 10);
+
+/*update usuario com n√∫mero do cart√£o*/
+UPDATE USUARIO SET numero_cartao='1111222233330000' WHERE id_usuario=1;
+UPDATE USUARIO SET numero_cartao='1111222233331111' WHERE id_usuario=2;
+UPDATE USUARIO SET numero_cartao='1111222233332222' WHERE id_usuario=3;
+UPDATE USUARIO SET numero_cartao='1111222233333333' WHERE id_usuario=4;
+UPDATE USUARIO SET numero_cartao='1111222233334444' WHERE id_usuario=5;
+UPDATE USUARIO SET numero_cartao='1111222233335555' WHERE id_usuario=6;
+UPDATE USUARIO SET numero_cartao='1111222233336666' WHERE id_usuario=7;
+UPDATE USUARIO SET numero_cartao='1111222233337777' WHERE id_usuario=8;
+UPDATE USUARIO SET numero_cartao='1111222233338888' WHERE id_usuario=9;
+UPDATE USUARIO SET numero_cartao='1111222233339999' WHERE id_usuario=10;
+
+/* pagamento */
+INSERT INTO PAGAMENTO (id_pagamento,numero_cartao, valor_pagamento, data_pagamento)
+    VALUES(1,'1111222233332222',100.00,'2018-01-08' ),
+    (2,'1111222233332222',100.00,'2018-02-08' ),
+    (3,'1111222233332222',100.00,'2018-03-08' ),
+    (4,'1111222233332222',100.00,'2018-04-08' ),
+    (5,'1111222233332222',100.00,'2018-05-08' ),
+    (6,'1111222233332222',100.00,'2018-06-08' ),
+    (7,'1111222233332222',100.00,'2018-07-08' ),
+    (8,'1111222233332222',100.00,'2018-08-08' ),
+    (9,'1111222233332222',100.00,'2018-09-08' ),
+    (10,'1111222233332222',100.00,'2018-10-08' );
+
+/* passagem */
+INSERT INTO PASSAGEM (id_passagem, id_usuario, numero_linha, id_pagamento, data, hora, valor)
+    VALUES(1,3,815,10,'2018-10-08','07:30:00', 3.40),
+    (2,3,815,10,'2018-10-08','12:00:00', 3.40),
+    (3,3,815,10,'2018-10-09','07:30:00', 3.40),
+    (4,3,815,10,'2018-10-09','12:00:00', 3.40),
+    (5,3,815,10,'2018-10-10','07:30:00', 3.40),
+    (6,3,815,10,'2018-10-10','12:00:00', 3.40),
+    (7,3,815,10,'2018-10-11','07:30:00', 3.40),
+    (8,3,815,10,'2018-10-11','12:00:00', 3.40),
+    (9,3,815,10,'2018-10-12','07:30:00', 3.40),
+    (10,3,815,10,'2018-10-12','12:00:00', 3.40);
    
 
  /*
@@ -456,10 +422,10 @@ ALTER TABLE USUARIO add numero_cartao char(16);
 UPDATE projeto set nome = 'Aguas limpas' where numero=25;
 SELECT titular,validade FROM cartao WHERE((numero = '1111222233338888' ) OR (titular LIKE '%Fernandes') OR (titular LIKE 'A%'));    
 UPDATE linha
-  SET desc_linha = REPLACE (desc_linha, 'T. LARANJEIRAS / T. IBES VIA 3™ PONTE/RETA DA PENHA','T.LARANJEIRAS / T. IBES VIA 3™ PONTE/RETA DA PENHA')
+  SET desc_linha = REPLACE (desc_linha, 'T. LARANJEIRAS / T. IBES VIA 3ÔøΩ PONTE/RETA DA PENHA','T.LARANJEIRAS / T. IBES VIA 3ÔøΩ PONTE/RETA DA PENHA')
   WHERE numero_linha = 507;
   
-alter table linha replace(desc_linha,'T. LARANJEIRAS / T. IBES VIA 3™ PONTE/RETA DA PENHA','T.LARANJEIRAS / T. IBES VIA 3™ PONTE/RETA DA PENHA');
+alter table linha replace(desc_linha,'T. LARANJEIRAS / T. IBES VIA 3ÔøΩ PONTE/RETA DA PENHA','T.LARANJEIRAS / T. IBES VIA 3ÔøΩ PONTE/RETA DA PENHA');
     
 SELECT numero_linha FROM linha WHERE desc_linha LIKE 'T.LARANJEIRAS%' OR desc_linha LIKE '%T.ITAPARICA VIA T.CARAPINA%';    
 SELECT desc_cidade FROM cidade where(id_cidade = 6);  
